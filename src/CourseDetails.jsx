0d488e3cf30df876de1740ca20bce8e2ce6ab966
tanwee14 <8409tan@gmail.com>
2025-07-05 20:37:16 +0530

    Updated CourseDetails

--- src/CourseDetails.jsx
+++ src/CourseDetails.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import Rating from './Rating';
+import { useState,useEffect } from 'react';
+import axios from "axios";
 
 function CourseDetails() {
-  const { id } = useParams();
-  const course = courseData.find((c) => c.id === id);
+  const {id} = useParams()  
+  const[course,setCourse]=useState(null)
+  const[loading,setLoading]=useState(true)
 
+  useEffect(()=>{
+    axios.get(`http://localhost:3000/courses/${id}`)
+    .then((res)=>{
+      setCourse(res.data)
+      setLoading(false)
+    })
+    .catch((err)=>{
+      console.log("error occured")
+      setLoading(true)
+    })
+
+  },[id])
+
+  if (loading) return <div>Loading...</div>;
   if (!course) return <div>Course not found</div>;
-
+  
   return (
     <div className="row my-1">
       <div className="col-md-9">
